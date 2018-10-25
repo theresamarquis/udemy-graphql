4bdfc81f476174e2f02d726062c5721f4d600da0
Theresa Marquis <tmarquis@kalmbach.com>
2018-10-25 16:58:04 -0500

    Initial commit

---
+++ schema/schema.js
@@ -0,0 +1,113 @@
+const graphql = require('graphql');
+const axios = require('axios');
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
+} = graphql;
+
+const CompanyType = new GraphQLObjectType({
+   name: 'Company',
+   fields: ()=> ({  // JS closure scope returning an object
+       id: {type: GraphQLString},
+       name: { type: GraphQLString},
+       description: { type: GraphQLString},
+       users: {
+           type: new GraphQLList(UserType),
+           resolve(parentValue, args) {
+               return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+               .then(response => response.data)
+           }
+       }
+   }) 
+})
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: ()=>({
+        id: {type: GraphQLString},
+        firstName: {type: GraphQLString},
+        age: {type: GraphQLInt},
+        company: { 
+            type: CompanyType,
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                .then(response => response.data);
+            }
+        }
+    })
+});
+
+
+
+// const fetchUserData = async (id) => {
+//     const url = `http://localhost:3000/users/${id}`;
+  
+//     try {
+//       const response = await fetch(url);
+//       if (!response.ok) {
+//         throw Error(`${response.status}`);
+//       }
+//       return await response.json();
+//     } catch (error) {
+//       throw Error(`Network request failed. (error: ${error.message})`);
+//     }
+// };
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: {id: {type: GraphQLString}},
+            resolve(parentValue, args){
+                // return _.find(users, { id: args.id})
+                // return users.find(user=>user.id === args.id) 
+                // resolve has to return a JS object but this find works!!
+                // future may need reduce?
+               
+                // const newObj = fetchUserData(args.id)
+                // return newObj
+
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then(response => response.data)
+           
+            }
+        },
+        company: {
+            type: CompanyType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args){
+                return axios.get(`http://localhost:3000/companies/${args.id}`)
+                .then(response => response.data);
+            }
+        }
+    }
+});
+
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+      addUser: {
+          type: UserType, //what you are going to return from the resolve fcn
+          args: {
+              firstName: { type: new GraphQLNonNull(GraphQLString) },
+              age: { type: new GraphQLNonNull(GraphQLInt) },
+              companyId: { type: GraphQLString}
+          },
+          resolve(parentValue, { firstName, age }) {
+            return axios.post(`http://localhost3000/users/`, { firstName, age })
+            .then(response => response.data)
+          }
+      }  
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query:  RootQuery,
+    mutation
+});
